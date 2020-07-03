f4a1815986e8c9255130ba5e043625712f53f120
kflansburg <kevin.flansburg@gmail.com>
2020-07-03 08:51:38 -0500

    Switch to BTreeMaps and fix crate docs.

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    tonic_build::compile_protos("proto/cri/v1alpha2/api.proto").unwrap();
+    let mut config = prost_build::Config::new();
+    config.btree_map(&["."]);
+    let proto = std::path::Path::new("proto/cri/v1alpha2/api.proto");
+    let proto_dir = proto
+        .parent()
+        .expect("proto file should reside in a directory");
+    tonic_build::configure()
+        .compile_with_config(config, &[proto], &[proto_dir])
+        .unwrap();
 }
