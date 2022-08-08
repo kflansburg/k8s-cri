b1e0d286990be98be9350b139576ed6f60a5b342
eliad-wiz <107865757+eliad-wiz@users.noreply.github.com>
2022-08-08 04:53:17 +0300

    add serde Serialize/Deserialize derives (#5)
    allow easily serializing/deserializing the
    various objects.

--- build.rs
+++ build.rs
@@ -1,4 +1,11 @@
 fn main() {
-    tonic_build::compile_protos("proto/v1.proto").unwrap();
-    tonic_build::compile_protos("proto/v1alpha2.proto").unwrap();
+    tonic_build::configure()
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile(&["proto/v1.proto"], &["proto/"])
+        .unwrap();
+
+    tonic_build::configure()
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile(&["proto/v1alpha2.proto"], &["proto/"])
+        .unwrap();
 }
