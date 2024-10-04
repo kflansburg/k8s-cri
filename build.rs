91e77928ab97e98b77214d4fec8e08950f482ddb
Aviram Hassan <aviramyhassan@gmail.com>
2024-10-04 14:17:40 +0300

    bump tonic,prost (#24)
    * bump versions
    
    * bump tonic,prost

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
 fn main() {
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .compile(&["proto/v1.proto"], &["proto/"])
+        .compile_protos(&["proto/v1.proto"], &["proto/"])
         .unwrap();
 
     tonic_build::configure()
         .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
-        .compile(&["proto/v1alpha2.proto"], &["proto/"])
+        .compile_protos(&["proto/v1alpha2.proto"], &["proto/"])
         .unwrap();
 }
